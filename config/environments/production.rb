7f23b8c4fd0202e992205fc5f116f7f70ef1cdbd
Mathieu Seguin <mathieu.seguin@gmail.com>
2015-11-09 15:46:34 +0800

    added region to fog config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
     storage: :fog,
     fog_credentials: {
       provider: 'AWS',
+      region: ENV['AWS_REGION'],
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
